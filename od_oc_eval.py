4e753b9387f6613054923e752686a71aa789ff27
yzc <yzc@qq.com>
2023-10-31 09:09:36 +0800

    add

--- od_oc_eval.py
+++ od_oc_eval.py
@@ -18,12 +18,12 @@ MODE = None
 
 
 def parse_args():
-    name = 'semi-drishti'
+    name = 'semi-REFUGE400'
     mode = 'semi'
 
     parser = argparse.ArgumentParser(description='SASS Framework')
     parser.add_argument('--resume_model', type=str,
-                        default='./exp/%s/resnet50_80.61.pth' % (name))
+                        default='./exp/%s/resnet50_86.14.pth' % (name))
     parser.add_argument('--config', type=str, default='./configs/%s' % (name + '.yaml'))
     parser.add_argument('--save-mask-path', type=str, default='exp/%s/predicts' % (name + '/' + mode))
     args = parser.parse_args()
@@ -59,6 +59,7 @@ def main(args):
     valloader = DataLoader(dataset, batch_size=1,
                            shuffle=False, pin_memory=True, num_workers=8, drop_last=False)
     tbar = tqdm(valloader)
+    background_binary_jaccard = JaccardIndex(num_classes=2, task='binary', average='micro').to('cuda:0')
     od_binary_jaccard = JaccardIndex(num_classes=2, task='binary', average='micro').to('cuda:0')
     oc_binary_jaccard = JaccardIndex(num_classes=2, task='binary', average='micro').to('cuda:0')
     cmap = color_map(cfg['dataset'])
@@ -66,6 +67,7 @@ def main(args):
     with torch.no_grad():
         for img, mask, id in tbar:
             img = img.cuda()
+            mask = mask.cuda()
             if cfg['dataset'] == 'cityscapes':
                 pred = pre_slide(model, img, num_classes=cfg['nclass'],
                                  tile_size=(cfg['crop_size'], cfg['crop_size']), tta=False)
@@ -73,6 +75,18 @@ def main(args):
                 pred = ms_test(model, img)
 
             pred = torch.argmax(pred, dim=1)
+
+            background_preds = copy.deepcopy(pred)
+            background_y = copy.deepcopy(mask)
+            background_preds[background_preds == 0] = 3
+            background_y[background_y == 0] = 3
+            background_preds[background_preds != 3] = 0
+            background_y[background_y != 3] = 0
+            background_preds[background_preds == 3] = 1
+            background_y[background_y == 3] = 1
+
+
+
             od_preds = copy.deepcopy(pred)
             od_y = copy.deepcopy(mask)
             od_preds[od_preds != 1] = 0
@@ -92,10 +106,14 @@ def main(args):
             od_cover_preds[od_cover_preds > 0] = 1
 
             od_binary_jaccard.update(od_cover_preds,od_cover_gt)
+            background_binary_jaccard.update(background_preds,background_y)
+#             od_binary_jaccard.update(od_preds,od_y)
             oc_binary_jaccard.update(oc_preds,oc_y)
+            background_IOU = background_binary_jaccard.compute()
             od_IOU = od_binary_jaccard.compute()
             oc_IOU = oc_binary_jaccard.compute()
-            mIOU = (od_IOU + oc_IOU) / 2
+
+            mIOU = (od_IOU + oc_IOU + background_IOU) / 3
             mIOU = mIOU
 
             tbar.set_description('OD_IOU: %.2f --OC_IOU:%.2f--mIOU: %.2f'% (od_IOU * 100.0,oc_IOU * 100.0,mIOU * 100.0))
@@ -104,7 +122,7 @@ def main(args):
             pred = Image.fromarray(pred, mode='P')
             pred.putpalette(cmap)
             pred.save('%s/%s' % (args.save_mask_path, os.path.basename(id[0].split(' ')[1])))
-
+    print("OD_IOU: %.2f --OC_IOU:%.2f"% (od_IOU * 100.0,oc_IOU * 100.0))
     mIOU *= 100.0
 
 
