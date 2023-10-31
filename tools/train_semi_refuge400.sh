f71090ff0658cfc1402e99f0ff41bd5efb2d1455
yzc <yzc@qq.com>
2023-10-31 10:04:40 +0800

    add

--- tools/train_semi_refuge400.sh
+++ tools/train_semi_refuge400.sh
@@ -2,10 +2,10 @@
 now=$(date +"%Y%m%d_%H%M%S")
 
 config=configs/semi-refuge400.yaml
-save_path=exp/semi-refuge400
+save_path=exp/semi-refuge400/version1
 
 mkdir -p $save_path
 python -W ignore train_semi_refuge400.py \
     --config=$config\
-    --save-path $save_path  --port 28990 | tee $save_path/$now.txt
+    --save-path $save_path  --port 28990 --info '伪标签来自teacher，没有对teacher的batchnorm进行有标记维护'
 
